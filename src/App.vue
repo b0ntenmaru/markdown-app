5ef6055e97b33e32b95a4bcdd67a99ba4bd85a15
b0ntenmaru <hiroaki.date@crowdworks.co.jp>
2019-09-05 23:34:40 +0900

    AppコンポーネントからHeader部分をコンポーネント化した

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <template>
   <v-app>
     <!-- header -->
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>MarkDown App</span>
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn @click="login" v-show="!$store.state.login_user">ログイン</v-btn>
-      <v-btn @click="logout" v-if="$store.state.login_user">ログアウト</v-btn>
-      <v-progress-linear
-      :active="loading"
-      :indeterminate="loading"
-      absolute bottom color="deep-purple accent-4"
-    />
-    </v-app-bar>
-
+    <TheHeader />
     <v-content>
       <!-- ここにコンポーネントを配置する -->
+      <!-- router-view はvue-routerに登録されていえるURLのコンポーネントを表示する -->
       <router-view></router-view>
     </v-content>
   </v-app>
@@ -28,32 +14,13 @@
 import Vue from 'vue';
 import { mapActions } from 'vuex';
 import firebase from 'firebase';
+import TheHeader from './components/TheHeader.vue';
 
 export default Vue.extend({
   name: 'App',
-  components: {},
-  data: () => ({
-    loading: true,
-  }),
-  created() {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setLoginUser(user);
-        this.loading = false;
-      } else {
-        this.deleteLoginUser();
-        this.loading = false;
-      }
-    });
+  components: {
+    TheHeader,
   },
-  watch: {
-    loading (val) {
-      if (!val) return
-      setTimeout(() => (this.loading = false), 3000)
-    },
-  },
-  methods: {
-    ...mapActions(['setLoginUser', 'login', 'logout', 'deleteLoginUser']),
-  },
+  data: () => ({}),
 });
 </script>
